8a2852cde1f31044e8ab2cb44efca294e6fd989d
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2025-07-02 14:10:13 -0600

    made computer choice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,8 +2,11 @@ const rock = 0;
 const paper = 1;
 const scissors = 2;
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3); // Will return a rang from 0 to 2
+  let computerChoice = Math.floor(Math.random() * 3);
 
   if (computerChoice === 0) {
     return "Rock";
@@ -17,3 +20,12 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+  let humanChoice = prompt(
+    "Hi user, Whats your choice, Rock, Paper or Scissor"
+  );
+  return console.log("you selected", humanChoice);
+}
+
+getHumanChoice();
